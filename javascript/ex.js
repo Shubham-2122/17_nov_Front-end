4d5124607393bfd954810da7ece8832173d5256e
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-12-16 11:53:24 +0530

    js

---
+++ javascript/ex.js
@@ -0,0 +1,20 @@
+console.log("hello external javascript")
+
+console.log(document.getElementById("data"))
+
+document.getElementById("data").innerHTML = "hello shubham"
+
+// BOM : browser object model 
+
+// 1) aelrt 
+// alert("pls field this form")
+
+// confirm("are your sure ?")
+
+// prompt("5+5 = ?")
+
+// print()
+
+/* 
+    multi line cmt
+*/
