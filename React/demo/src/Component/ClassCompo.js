bfc6cf17d24430d87289a3e4b32fb96cf8b586c3
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-01-23 11:05:08 +0530

    react

---
+++ React/demo/src/Component/ClassCompo.js
@@ -0,0 +1,46 @@
+// component : it's block of code when we need use it call it
+// class vs function
+// class : compoenent extends , reder data
+// state full
+
+import React, { Component } from "react";
+
+class ClassCompo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello this Class Compoenent</h1>
+                <p>lorem sdajyg</p>
+            </div>
+        )
+    }
+}
+
+export default ClassCompo
+
+
+// import React, { Component } from 'react'
+
+// class ClassCompo extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>hello rce (react class Export componenet) Compoenent</h1>
+//       </div>
+//     )
+//   }
+// }
+
+// export default ClassCompo
+
+// import React, { Component } from 'react'
+
+// export default class ClassCompo extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello this RCC compoenet</h1>
+//       </div>
+//     )
+//   }
+// }
---
+++ React/demo/src/Component/FuncCompo.js
@@ -0,0 +1,70 @@
+// // component : it's block of code when we need use it call it
+// normal function 
+// stateless function
+
+import React from "react";
+
+function FuncCompo(){
+    return(
+        <div>
+            <h1>Hello this Function compoenet</h1>
+        </div>
+    )
+}
+
+// const FuncCompo=()=>{
+//     return(
+//         <div>
+//             <h1>Hello this Arrow Function compoenent</h1>
+//         </div>
+//     )
+// }
+
+export default FuncCompo;
+
+// import React from 'react'
+
+// function FuncCompo() {
+//   return (
+//     <div>
+//       <h1>Hello this Function RFCE</h1>
+//     </div>
+//   )
+// }
+
+// export default FuncCompo
+
+// import React from 'react'
+
+// export default function FuncCompo() {
+//   return (
+//     <div>
+//       <h1>Hello this Fuction RFC</h1>
+//     </div>
+//   )
+// }
+
+// import React from 'react'
+
+// const FuncCompo = () => {
+//   return (
+//     <div>
+//       <h1>Hello this React arrow RAFCE</h1>
+//     </div>
+//   )
+// }
+
+// export default FuncCompo
+
+// import React from 'react'
+
+// export default FuncCompo = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+
+
